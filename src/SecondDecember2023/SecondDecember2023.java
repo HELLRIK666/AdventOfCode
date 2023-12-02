57ba4138bbb34f090cd76bf02cc4d6d26fb6775a
hljo <hljo@itu.dk>
2023-12-02 14:34:30 +0100

    Work on 2. Dec

--- src/SecondDecember2023/SecondDecember2023.java
+++ src/SecondDecember2023/SecondDecember2023.java
@@ -1,8 +1,23 @@
 package SecondDecember2023;
 
+import java.util.Scanner;
+
 public class SecondDecember2023 {
 
+    static Scanner s = new Scanner(System.in);
+    int redMax = 12;
+    int greenMax = 13;
+    int blueMax = 14;
+
+    public static void logic() {
+        for (int i = 0; i < 5; i++) {
+            String str = s.nextLine();
+            System.out.println(str);
+        }
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world test2!");
+        logic();
     }
 }
