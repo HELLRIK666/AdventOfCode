7eddedbbe15d0ffff5233f4f714e7e8c8cddc046
hljo <hljo@itu.dk>
2023-12-02 13:51:34 +0100

    Getting package structure

---
+++ src/FirstDecember2023/FirstDecember2023.java
@@ -0,0 +1,9 @@
+package FirstDecember2023;
+
+public class FirstDecember2023 {
+
+        public static void main(String[] args) {
+            System.out.println("Hello world test!");
+        }
+
+}
---
+++ src/SecondDecember2023/SecondDecember2023.java
@@ -0,0 +1,8 @@
+package SecondDecember2023;
+
+public class SecondDecember2023 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world test2!");
+    }
+}
