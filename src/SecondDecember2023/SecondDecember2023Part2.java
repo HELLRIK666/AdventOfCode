d603a71f23a3b7265b141b4bd9fbf86e916249ed
hljo <hljo@itu.dk>
2023-12-04 20:22:08 +0100

    Work on 2. Dec done

--- src/SecondDecember2023/SecondDecember2023Part2.java
+++ src/SecondDecember2023/SecondDecember2023Part2.java
@@ -1,80 +1,85 @@
 package SecondDecember2023;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class SecondDecember2023Part2 {
 
     static Scanner s = new Scanner(System.in);
-    static int redMax = 12;
-    static int greenMax = 13;
-    static int blueMax = 14;
     static int result = 0;
+
 
     public static void logic() {
         for (int i = 0; i < 100; i++) {
-            boolean bool = true;
             String str = s.nextLine();
-            System.out.println(str);
-
-
 
             String strWithoutGame = str.substring(5);
-            System.out.println(strWithoutGame);
 
             String[] tokens1 = strWithoutGame.split("[:]");
-            System.out.println("Tokens1 - del 1: " + tokens1[0]);
-            System.out.println("Tokens1 - del 2: " + tokens1[1]);
 
-            int id = Integer.parseInt(tokens1[0]);
             strWithoutGame = tokens1[1];
-            System.out.println("strWithoutGame: " + strWithoutGame);
-            System.out.println("id: " + id);
+
             String[] tokens = strWithoutGame.split("[,;]");
+            int powerRed = 0;
+            int powerGreen = 0;
+            int powerBlue = 0;
+            ArrayList<Integer> redList = new ArrayList<Integer>();
+            ArrayList<Integer> greenList = new ArrayList<Integer>();
+            ArrayList<Integer> blueList = new ArrayList<Integer>();
+
 
             for (String t : tokens) {
                 System.out.println("t: " + t);
-                if (t.contains("red") && t.length()==6) {
-                    if (Integer.parseInt(t.substring(1,2)) > redMax) {
-                        System.out.println("t.substring(1,2) : " + t.substring(1,2));
-                        bool = false;
-                        //result = result + id;
-                    }
+                if (t.contains("red") && t.length() == 7) {
+
+                    powerRed = Integer.parseInt(t.substring(1, 3));
+
+                    redList.add(powerRed);
+
                 }
-                if (t.contains("red") && t.length()==7) {
-                    if (Integer.parseInt(t.substring(1,3)) > redMax) {
-                        System.out.println("t.substring(1,3) : " + t.substring(1,3));
-                        bool = false;
-                        //result = result + id;
-                    }
+                if (t.contains("red") && t.length() == 6) {
+
+
+                    powerRed = Integer.parseInt(t.substring(1, 2));
+
+                    redList.add(powerRed);
+
                 }
-                if (t.contains("green") && t.length()==8) {
-                    if (Integer.parseInt(t.substring(1,2)) > greenMax) {
-                        bool = false;
-                        //result = result + id;
-                    }
+                if (t.contains("green") && t.length() == 9) {
+
+                    powerGreen = Integer.parseInt(t.substring(1, 3));
+
+                    greenList.add(powerGreen);
+
                 }
-                if (t.contains("green") && t.length()==9) {
-                    if (Integer.parseInt(t.substring(1,3)) > greenMax) {
-                        bool = false;
-                        //result = result + id;
-                    }
+                if (t.contains("green") && t.length() == 8) {
+
+                    powerGreen = Integer.parseInt(t.substring(1, 2));
+
+                    greenList.add(powerGreen);
                 }
-                if (t.contains("blue") && t.length()==7) {
-                    if (Integer.parseInt(t.substring(1,2)) > blueMax) {
-                        bool = false;
-                        //result = result + id;
-                    }
+                if (t.contains("blue") && t.length() == 8) {
+
+                    powerBlue = Integer.parseInt(t.substring(1, 3));
+
+                    blueList.add(powerBlue);
                 }
-                if (t.contains("blue") && t.length()==8) {
-                    if (Integer.parseInt(t.substring(1,3)) > blueMax) {
-                        bool = false;
-                        //result = result + id;
-                    }
+                if (t.contains("blue") && t.length() == 7) {
+                    powerBlue = Integer.parseInt(t.substring(1, 2));
+
+                    blueList.add(powerBlue);
                 }
 
+
             }
-            System.out.println(bool);
-            if (bool) {result = result + id;}
+            powerRed = Collections.max(redList);
+            powerGreen = Collections.max(greenList);
+            powerBlue = Collections.max(blueList);
+
+            result = result + (powerRed * powerGreen * powerBlue);
+
+
         }
         System.out.println("Result: " + result);
     }
